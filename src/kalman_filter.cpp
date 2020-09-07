14ca25950aa79a23791927d61bce3bb95a162655
Samah <samah@Samah-MacBook-Pro.local>
2020-09-07 12:00:19 -0700

    init commit.

---
+++ src/kalman_filter.cpp
@@ -0,0 +1,95 @@
+#include "kalman_filter.h"
+#include <math.h>
+#include <iostream>
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+using std::cout;
+using std::endl;
+/* 
+ * Please note that the Eigen library does not initialize 
+ *   VectorXd or MatrixXd objects with zeros upon creation.
+ */
+
+KalmanFilter::KalmanFilter() {}
+
+KalmanFilter::~KalmanFilter() {}
+
+void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
+                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
+  x_ = x_in;
+  P_ = P_in;
+  F_ = F_in;
+  H_ = H_in;
+  R_ = R_in;
+  Q_ = Q_in;
+}
+
+void KalmanFilter::Predict() {
+  /**
+   * TODO: predict the state
+   */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
+}
+
+void KalmanFilter::Update(const VectorXd &z) {
+  /**
+   * TODO: update the state by using Kalman Filter equations
+   */
+   
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+}
+
+void KalmanFilter::UpdateEKF(const VectorXd &z) {
+  /**
+   * TODO: update the state by using Extended Kalman Filter equations
+   */
+   
+  // convert x' from cartesian to polar coordinates to calculate y.
+  // recover state parameters
+  float px = x_(0);
+  float py = x_(1);
+  float vx = x_(2);
+  float vy = x_(3);
+  
+  // compute rho, phi, and rho_dot
+  float rho = sqrt(px*px+py*py);
+  float phi = atan2(py,px);
+  float rho_dot = (px * vx + py * vy) / rho;
+  
+  // Calculate h(x')
+  VectorXd h(3);
+  h << rho, phi, rho_dot;
+  
+  VectorXd y = z - h;
+  
+  // normalize the angle phi in the y vector to make sure it's value between -pi and pi.
+  double pi = 3.14159265;
+  y(1) = fmod(y(1), pi);
+  
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+  
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+  
+}
